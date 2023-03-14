1606627a04e4cff6a40facd0540e254460732142
colin <beutt102@mail.chapman.edu>
2023-03-14 15:17:56 -0700

    made on Click for preview button

--- app/src/main/java/com/colinbeuttler/mypromoapp/MainActivity.kt
+++ app/src/main/java/com/colinbeuttler/mypromoapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.colinbeuttler.mypromoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val previewButton: Button = findViewById(R.id.preview_button)
+        previewButton.setOnClickListener {
+            onPreviewClicked()
+
+        }
+    }
+
+    private fun onPreviewClicked() {
+        val contactName: TextInputEditText = findViewById(R.id.edit_text_contact_name)
+        val contactNumber: TextInputEditText = findViewById(R.id.edit_text_contact_number)
+        val textString = contactName.text.toString() + ", " + contactNumber.text.toString()
+        Toast.makeText(this, textString, Toast.LENGTH_LONG).show()
     }
 }
