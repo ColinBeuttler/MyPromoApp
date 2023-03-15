4626e471383f625da0d4047e087979279281bb89
colin <beutt102@mail.chapman.edu>
2023-03-15 10:22:01 -0700

    created toast for preview button

--- app/src/main/java/com/colinbeuttler/mypromoapp/MainActivity.kt
+++ app/src/main/java/com/colinbeuttler/mypromoapp/MainActivity.kt
@@ -13,7 +13,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,12 +24,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onPreviewClicked() {
-       val contactName = edit_text_contact_name.text.toString()
-        val contactNumebr = edit_text_contact_number.text.toString()
+        val contactName = edit_text_contact_name.text.toString()
+        val contactNumber = edit_text_contact_number.text.toString()
         val displayName = edit_text_my_display_name.text.toString()
         val includeJunior = check_box_junior.isChecked
-        val jobTitle = spinner_job_title.selectedItem.toString()
+        val jobTitle = spinner_job_title.selectedItem?.toString()
         val immediateStart = check_box_immediate_start.isChecked
         val startDate = edit_text_start_date.text.toString()
+        val testString = "Name of Contact: $contactName, Contact Phone Number: $contactNumber, My display Name: $displayName, Include Junior: $includeJunior, Job Title: $jobTitle, Start Immediately: $immediateStart, Start Date: $startDate"
+
+        Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
     }
 }
