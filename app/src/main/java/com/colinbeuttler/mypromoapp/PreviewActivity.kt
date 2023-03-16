5ecb0b9273c229b4240ab9d24b1c211819468525
colin <beutt102@mail.chapman.edu>
2023-03-16 14:36:15 -0700

    added sms message functionality

--- app/src/main/java/com/colinbeuttler/mypromoapp/PreviewActivity.kt
+++ app/src/main/java/com/colinbeuttler/mypromoapp/PreviewActivity.kt
@@ -1,5 +1,7 @@
 package com.colinbeuttler.mypromoapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,25 +9,46 @@ import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_preview.*
 
 class PreviewActivity : AppCompatActivity() {
+
+    private var message: Message? = null
+    private lateinit var messagePreviewText: String
+
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
-        val message = intent.getSerializableExtra("Message", Message::class.java)
-        val messagePreviewText = """
-            Hi ${message?.contactName},
-            
-            My name is ${message?.displayName} and I am ${message?.getFullJobDescription()}
-            
-            I have a portfolio of apps to demonstrate my technical skills that I can show on request.
-            
-            I am able to start a new position ${message?.getAvailablity()}.
-            
-            Please get in touch if you have any suitable roles for me.
-            
-            Thanks and regards.
-            
-        """.trimIndent()
+        displayMessage()
+        setUpButton()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun displayMessage() {
+        message = intent.getSerializableExtra("Message", Message::class.java)
+        messagePreviewText = """
+                Hi ${message?.contactName},
+                
+                My name is ${message?.displayName} and I am ${message?.getFullJobDescription()}
+                
+                I have a portfolio of apps to demonstrate my technical skills that I can show on request.
+                
+                I am able to start a new position ${message?.getAvailablity()}.
+                
+                Please get in touch if you have any suitable roles for me.
+                
+                Thanks and regards.
+                
+            """.trimIndent()
         text_view_message.text = messagePreviewText
+    }
+
+    private fun setUpButton() {
+        send_message_button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("smsto: ${message?.contactNumber}")
+                putExtra("sms_body", messagePreviewText)
+            }
+            startActivity(intent)
+        }
+
     }
 }
